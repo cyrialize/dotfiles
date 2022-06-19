7f1f4ac68d151d138adeaa8bf7a792586bde447e
Jonny Cyr <jonny@Jonnys-MacBook-Pro.local>
2022-06-19 11:12:15 -0400

    first commit

---
+++ setup.sh
@@ -0,0 +1,24 @@
+# Setup script for setting up my dotfiles
+# Meant for MacOS or Linux
+
+# Set up the symlink to my file
+# For everything else, I just use `source``
+if [[ ! -L $HOME/.zshrc ]]; then
+    ln -s $HOME/Code/dotfiles/zshrc $HOME/.zshrc
+fi
+
+# Pull in and install the pure prompt
+# https://github.com/sindresorhus/pure
+if [[ ! -d $HOME/.zsh/pure ]]; then
+    mkdir -p "$HOME/.zsh"
+    git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+fi
+
+# Pull in and install the base16 shell theme
+# https://github.com/base16-project/base16-shell
+if [[ ! -d $HOME/.config/base16-shell ]]; then
+    git clone https://github.com/base16-project/base16-shell.git ~/.config/base16-shell
+fi
+
+brew install fortune
+brew install cowsay
