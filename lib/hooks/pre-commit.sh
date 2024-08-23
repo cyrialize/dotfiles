016ccca431dc4d7d69fa72fff7e6a36571b01182
Jonny Cyr <jonathancyr@fastmail.com>
2024-08-22 22:42:08 -0400

    Updated pre commit file

--- lib/hooks/pre-commit.sh
+++ lib/hooks/pre-commit.sh
@@ -3,13 +3,10 @@
 find $HOME/Code/dotfiles -name '*.sh' -exec shellcheck {} +
 result=$?
 
-echo
-echo "RESULT:"
-echo "-----------------------"
 if [[ $result -eq 0 ]]; then
-  echo "All checks passed!"
+  echo "All shellchecks passed!"
+  exit 0
 else
-  echo "Checks have failed!"
+  echo "Shellchecks have failed!"
+  exit 1
 fi
-echo "-----------------------"
-echo
